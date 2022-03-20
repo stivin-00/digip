9d9534b8c8b1e134fb345f75ee2af51275591f53
stivin-00 <68236676+stivin-00@users.noreply.github.com>
2022-03-20 19:50:13 +0100

    #

--- src/App.js
+++ src/App.js
@@ -1,8 +1,10 @@
 import React from "react";
+import Navbar from "./components/Navbar";
 
 function App() {
   return (
     <div className="App">
+      <Navbar/>
       <h1>hello </h1>
     </div>
   );
---
+++ src/components/Navbar.js
@@ -0,0 +1,22 @@
+import React from "react";
+
+const Navbar = () => {
+  return (
+    <div className="navbar">
+      <div className="top-nav">
+        <div className="logo">digipol</div>
+        <div className="tel">call +234 08123583317 </div>
+      </div>
+      <hr></hr>
+      <div className="bottom-nav">
+        <div className="nav-menu">home</div>
+        <div className="nav-menu">service</div>
+        <div className="nav-menu">insight</div>
+        <div className="nav-menu">about us</div>
+        <div className="nav-menu">contact</div>
+      </div>
+    </div>
+  );
+};
+
+export default Navbar;
